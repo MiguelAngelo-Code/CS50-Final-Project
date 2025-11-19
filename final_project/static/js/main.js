7c3513f777d6ed7f728317b2f71de1df974de53e
Migue Lopes <miguellopes121@gmail.com>
2025-11-19 16:05:56 +0100

    User now able to edit all fields from manage transactions page. Cancel button no enale to togle back to regular view

--- final_project/static/js/main.js
+++ final_project/static/js/main.js
@@ -1,19 +1,91 @@
 
+
 function startEdit(id) {
    
-    // todo:
-    // Account 
+    // Hide/show <td> fileds
+
+    // Account
+    document.getElementById(`view-acc-${id}`).style.display = "none";
+    document.getElementById(`edit-acc-${id}`).style.display = "inline-block";
+
     // Type
+    document.getElementById(`view-type-${id}`).style.display = "none";
+    document.getElementById(`edit-type-${id}`).style.display = "inline-block";
+
     // Category
+    document.getElementById(`view-cat-${id}`).style.display = "none";
+    document.getElementById(`edit-cat-${id}`).style.display = "inline-block";
+
     // Date
+    document.getElementById(`view-date-${id}`).style.display = "none";
+    document.getElementById(`edit-date-${id}`).style.display = "inline-block";
 
     // Amount
     document.getElementById(`view-amt-${id}`).style.display = "none";
     document.getElementById(`edit-amt-${id}`).style.display = "inline-block";
 
-    // Buttons
+    // Hide delete/edit buttons
+    document.getElementById(`del-btn-${id}`).style.display = "none";
     document.getElementById(`edit-btn-${id}`).style.display = "none";
+
+    // Show save/cancel buttons
     document.getElementById(`save-btn-${id}`).style.display = "inline-block";
+    document.getElementById(`cancel-btn-${id}`).style.display = "inline-block";
+
+    // disable dropdown buttons outside of edit row
+    var elements = document.getElementsByName('drop-btn');
+
+    for (var i = 0; i < elements.length; i++){
+
+        if (elements[i].id !== `drop-btn-${id}`){
+            elements[i].disabled = true;
+        } 
+    }
 }
 
-// todo cancle edit function
+
+// Cancle edit
+function endEdit(id) {
+
+    // Fields
+
+    // Account
+    document.getElementById(`view-acc-${id}`).style.display = "inline-block";
+    document.getElementById(`edit-acc-${id}`).style.display = "none";
+
+    // Type
+    document.getElementById(`view-type-${id}`).style.display = "inline-block";
+    document.getElementById(`edit-type-${id}`).style.display = "none";
+
+    // Category
+    document.getElementById(`view-cat-${id}`).style.display = "inline-block";
+    document.getElementById(`edit-cat-${id}`).style.display = "none";
+
+    // Date
+    document.getElementById(`view-date-${id}`).style.display = "inline-block";
+    document.getElementById(`edit-date-${id}`).style.display = "none";
+
+    // Amount
+    document.getElementById(`view-amt-${id}`).style.display = "inline-block";
+    document.getElementById(`edit-amt-${id}`).style.display = "none";
+
+    // Show delete/edit buttons
+    document.getElementById(`del-btn-${id}`).style.display = "inline-block";
+    document.getElementById(`edit-btn-${id}`).style.display = "inline-block";
+
+    // Hide save/cancel buttons
+    document.getElementById(`save-btn-${id}`).style.display = "none";
+    document.getElementById(`cancel-btn-${id}`).style.display = "none";
+
+    // Re-enable dropdown buttons
+    var elements = document.getElementsByName('drop-btn');
+
+    for (var i = 0; i < elements.length; i++){
+
+        elements[i].disabled = false;
+        
+    }
+
+}
+
+// function 
